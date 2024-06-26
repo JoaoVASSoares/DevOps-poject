68705e47639cec341f35e104d97299c8f3ae7066
João Vitor <joaovitoralmeidas@hotmail.com>
2024-06-25 22:51:57 -0300

    novo teste

--- pull-server.sh
+++ pull-server.sh
@@ -4,7 +4,7 @@ read message
 echo "Mensagem do commit: "
 git commit -am "$message"
 git push
-ssh ssh joaovitoralmeidas@34.151.221.54 \
+ssh joaovitoralmeidas@34.151.221.54 \
     'git -C /home/joaovitoralmeidas/devOps/DevOps-poject/ ' \
     'git pull && ' \
     'pm2 restart 0 && sudo systemctl restart nginx'
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Olá Kellinha, Eu te amo muito <3 <3 <3!';
+    return 'Olá Kellinha, Eu te amo muito <3 <3 <3! TESTE 01';
   }
 }
