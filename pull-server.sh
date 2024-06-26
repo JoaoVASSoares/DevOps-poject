5846c02321a669cdb9ae7991ef2a7a06000755dd
João Vitor <joaovitoralmeidas@hotmail.com>
2024-06-25 22:57:22 -0300

    teste novo 02

--- pull-server.sh
+++ pull-server.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+
+# Adicionar todas as mudanças ao git
 git add .
+
+# Ler a mensagem do commit
+echo "Digite a mensagem do commit: "
 read message
-echo "Mensagem do commit: "
+
+# Fazer o commit com a mensagem fornecida
+echo "Mensagem do commit: $message"
 git commit -am "$message"
+
+# Fazer push das mudanças para o repositório remoto
 git push
-ssh joaovitoralmeidas@34.151.221.54 \
-    'cd devOps/DevOps-poject/ ' \
-    'git pull' \
-    'npm run build' \
-    'pm2 restart 0 && sudo systemctl restart nginx'
+
+# Executar comandos no servidor remoto via SSH
+ssh joaovitoralmeidas@34.151.221.54 << EOF
+    cd /home/joaovitoralmeidas/devOps/DevOps-poject/
+    git pull
+    npm run build
+    pm2 restart 0
+    sudo systemctl restart nginx
+EOF
