2b755920a13da690b58dd0d41c4094a69143f7fd
João Vitor <joaovitoralmeidas@hotmail.com>
2024-06-25 22:13:16 -0300

    📝: Edit teste

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Olá Kellinha, Eu te amo muito <3 <3 <3!';
   }
 }
