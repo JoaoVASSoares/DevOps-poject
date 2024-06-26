362d92ef704e6e224bad9a1cc4fc9bf36d38e49a
João Vitor <joaovitoralmeidas@hotmail.com>
2024-06-25 23:12:53 -0300

    ok

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Renam minha aplição esta no ar e funcionando muito bem! Teste 05";
+    return "Maquiagem da boca rosa";
   }
 }
