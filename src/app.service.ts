2c762a9c40b6e776dfc8ab0f4fb21416be1d2243
João Vitor <joaovitoralmeidas@hotmail.com>
2024-06-25 22:58:15 -0300

    Renam teste

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Olá Kellinha, Eu te amo muito <3 <3 <3! TESTE 01';
+    return "Renam minha aplição esta no ar e funcionando muito bem!";
   }
 }
