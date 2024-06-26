709a825ac89e305de89eba74787ef961f988d7ca
João Vitor <joaovitoralmeidas@hotmail.com>
2024-06-25 22:52:43 -0300

    build test

--- pull-server.sh
+++ pull-server.sh
@@ -6,5 +6,5 @@ git commit -am "$message"
 git push
 ssh joaovitoralmeidas@34.151.221.54 \
     'git -C /home/joaovitoralmeidas/devOps/DevOps-poject/ ' \
-    'git pull && ' \
+    'git pull && npm build' \
     'pm2 restart 0 && sudo systemctl restart nginx'
