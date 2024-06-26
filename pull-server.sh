24f6a7313b4e41646a9d71836bf2b6ff4ad8a5a8
João Vitor <joaovitoralmeidas@hotmail.com>
2024-06-25 22:50:52 -0300

    teste

---
+++ pull-server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git add .
+read message
+echo "Mensagem do commit: "
+git commit -am "$message"
+git push
+ssh ssh joaovitoralmeidas@34.151.221.54 \
+    'git -C /home/joaovitoralmeidas/devOps/DevOps-poject/ ' \
+    'git pull && ' \
+    'pm2 restart 0 && sudo systemctl restart nginx'
