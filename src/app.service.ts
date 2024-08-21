ffac527493383a287d55fd3d955bcb9c616f1ae4
João Vitor <joaovitoralmeidas@hotmail.com>
2024-08-21 20:41:59 -0300

    📝: Dockerfile create

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Renam minha aplicação esta completamente funcional !! GOOGLE CLOUD";
+    return "Renam esta é minha aplicação funcionando em um container docker!!";
   }
 }
